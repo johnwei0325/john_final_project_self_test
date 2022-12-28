9d46ab0a222c5a931b4b65081f46120ac237d1c2
Pinru Huang <b10901171@g.ntu.edu.tw>
2022-12-28 22:14:58 +0800

    Add some stations

--- NTU_Biking/frontend/src/containers/NearestStations.js
+++ NTU_Biking/frontend/src/containers/NearestStations.js
@@ -6,20 +6,25 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import EE2_Building_Southside from "./images/Stations/CSIE_Der_Tain_Hall.jpg"
 
 const stations = [
     {label: '1st Student Activity Center 第一學生活動中心', dist: 200},
     {label: 'Astronomy Mathematics Building 天文數學館', dist: 1500},
-    {label: 'Barry Lam Hall 博理館', dist: 300},
+    {label: 'Barry Lam Hall 博理館', dist: 300, src: "/static/media/Barry_Lam_Hall.450fe36207c086a7f957.PNG",
+    location:{latitude: 25.019217, longitude: 121.542462}},
     {label: 'Boya Lecture Building 博雅教學館', dist: 1200},
     {label: 'Center for Condensed Matter Sciences 凝態科學中心/物理系', dist: 1700},
     {label: 'College of Liberal Arts 文學院', dist: 900},
-    {label: 'CSIE Der Tain Hall 德田館(資工系)', dist: 250},
+    {label: 'CSIE Der Tain Hall 德田館(資工系)', dist: 250, src: "/static/media/CSIE_Der_Tain_Hall.58c8386d988cea018508.jpg",
+    location:{latitude: 25.019448298802605, longitude: 121.54144852479706}},
     {label: 'CSIE Der Tain Hall Northside 德田館(資工系)北側', dist: 350},
     {label: 'Department of Psychology North Hall 心理系北館', dist: 500},
     {label: 'Department of Psychology South Hall 心理系南館', dist: 390},
-    {label: 'EE-2 building Southside 電機二館南側', dist: 50},
-    {label: 'EE-2 building Northside 電機二館北側', dist: 260},
+    {label: 'EE-2 building Southside 電機二館南側', dist: 50, src: "/static/media/EE-2_Building_Southside.28baf89d8937375365f0.jpg",
+    location:{latitude: 25.018467, longitude: 121.542027}},
+    {label: 'EE-2 building Northside 電機二館北側', dist: 260,
+    location:{latitude: 25.019135, longitude: 121.541993}},
     {label: 'Gontong Lecture Building 共同教學館', dist: 1900},
     {label: 'Main Library 總圖書館', dist: 200},
     {label: 'Ming Dar Hall 明達館', dist: 270},
@@ -36,23 +41,25 @@ function NearestStations () {
         <List sx={{ width: '100%', maxWidth: `calc(0.8*vw)`, bgcolor: 'background.paper' }}>
             {stations.map((stop, index) => 
             <>
-            <ListItem alignItems="flex-start" sx={{height: '80px'}}>
-                <ListItemAvatar>
-                    <Avatar alt="Remy Sharp" src={stop.src} />
+            <ListItem alignItems="flex-start" justifyItems='center' sx={{height: '116px'}}>
+                <ListItemAvatar sx={{height: '80px', width: '100px'}} >
+                    <Avatar alt="Remy Sharp" src={stop.src} sx={{height: '80px', width: '80px'}}/>
+                    {EE2_Building_Southside}
                 </ListItemAvatar>
                 <ListItemText
                 primary={stop.label}
+                primaryTypographyProps={{variant: 'h5'}}
                 secondary={
                     <React.Fragment>
                     <Typography
                         sx={{ display: 'inline' }}
                         component="span"
-                        variant="body2"
+                        variant="h6"
                         color="text.primary"
                     >
-                       Distance: &nbsp;
+                       Distance: &nbsp; 
+                       <span style={{color: 'gray'}}> {`${stop.dist} meters`} </span>
                     </Typography>
-                    {`${stop.dist} meters`}
                     </React.Fragment>
                 }
                 />
